b1f75e27849952c533828a9a6a9566a1a59b2fe3
user <user@DESKTOP-RH0EED9>
2023-07-25 22:48:23 +0900

    2023-07-25 처음부터 코드를  다시 해야 한다ㅠ

--- Project7/FileName.c
+++ Project7/FileName.c
@@ -2,21 +2,25 @@
 
 int main()
 {
-	int a, b, c, d;
+	int a, b, c, d, e;
+	a = 0;
+	b = 0;
+	c = 0;
 	printf("삼각형 한변의 길이");
 	scanf("%d", &d);
-	for (a = 0; a <= d; a++)
+	for (a = 1; a <= d; a++)
 	{
-		for (b = 0; b <= a; b++)
+		for (b = 1; b <=a; b++)
 		{
 			printf("*");
 		}
-		printf("\n");
-	}
-	for (a = 0; a < d; a++)
-	{
-		for (b = 0; b < d-a; b++)
+		for (c = d - a; c >= 0; c--)
 		{
+			printf(" ");
+		}
+		for (e = d - a; e <= 0; c--)
+
+		{ 
 			printf("*");
 		}
 		printf("\n");
