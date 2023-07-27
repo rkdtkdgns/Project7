468d85bce2194963180205ffe4e0aab94847596d
user <user@DESKTOP-RH0EED9>
2023-07-27 21:09:31 +0900

    2023-07-27

--- Project7/FileName.c
+++ Project7/FileName.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
 	int a, b, c, d, e;
 	a = 0;
 	b = 0;
 	c = 0;
 	printf("삼각형 한변의 길이");
 	scanf("%d", &d);
-	for (a = 1; a < d; a++)
+	for (a = 1; a <= d; a++)
 	{
 		for (b = 1; b <= a; b++)
 		{
 			printf("*");
 		}
-		for (c = d; c <= 0; c--)
+		for (c = d; c <= 0; c++)
 		{
 			printf(" ");
-		}
+		} 
 		printf("\n");
-	
-		for (b = 1; b <= b - 1; b++)
-		{
-			printf(" ");
-		}
-		for (c = d; c > a; c--)
+		for (c = d; c > a+1; c--)    
 		{
 			printf("*");
+		}
+		for (b = 1; b <= a; b++)
+		{
+			printf(" ");
 		}
 	}
 	return 0;
